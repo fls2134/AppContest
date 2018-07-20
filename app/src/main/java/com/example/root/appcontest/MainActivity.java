223459cd9dbd1c8dd1ec069270919c830678466c
mixify <mixify@naver.com>
2018-07-20 14:22:13 +0900

    Merging map activity completed ------------------------------ still has issues - location permission is got weirdly - needs to change some icons... drawables

--- app/src/main/java/com/example/root/appcontest/MainActivity.java
+++ app/src/main/java/com/example/root/appcontest/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.root.appcontest;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,6 +16,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity
@@ -46,6 +50,26 @@ public class MainActivity extends AppCompatActivity
         /* navigation view */
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        /* maps activity button */
+        Button mapsButton = (Button) findViewById(R.id.button2);
+        mapsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest, Manifest.permission.ACCESS_FINE_LOCATION);
+                if(permissionCheck == PackageManager.PERMISSION_DENIED)
+                {
+
+                }
+                else
+                {
+
+                }
+
+                Intent mapsIntent = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(mapsIntent);
+            }
+        });
     }
 
     @Override
