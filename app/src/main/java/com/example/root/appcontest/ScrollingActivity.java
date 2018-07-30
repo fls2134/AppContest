199fe6e59b060fe4dc75a215771b168402abe3f0
fls2134 <fls213444@gmail.com>
2018-07-30 11:28:05 +0900

    test recycler view

--- app/src/main/java/com/example/root/appcontest/ScrollingActivity.java
+++ app/src/main/java/com/example/root/appcontest/ScrollingActivity.java
@@ -1,15 +1,33 @@
 package com.example.root.appcontest;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.TextView;
+import com.example.root.appcontest.Item;
+
+import java.util.ArrayList;
 
 public class ScrollingActivity extends AppCompatActivity {
+    Context mContext;
+
+    RecyclerView recyclerView;
+    RecyclerView.Adapter Adapter;
+    RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +44,76 @@ public class ScrollingActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        mContext = getApplicationContext();
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+
+        ArrayList items = new ArrayList<>();
+
+        items.add(new Item(R.drawable.hubble_ultra_deep_field, "hubble"));
+        items.add(new Item(R.drawable.hubble_ultra_deep_field, "hubble"));
+        items.add(new Item(R.drawable.hubble_ultra_deep_field, "hubble"));
+        items.add(new Item(R.drawable.hubble_ultra_deep_field, "hubble"));
+        items.add(new Item(R.drawable.hubble_ultra_deep_field, "hubble"));
+        items.add(new Item(R.drawable.hubble_ultra_deep_field, "hubble"));
+        items.add(new Item(R.drawable.hubble_ultra_deep_field, "hubble"));
+        items.add(new Item(R.drawable.hubble_ultra_deep_field, "hubble"));
+
+        layoutManager = new LinearLayoutManager(this);
+        //layoutManager = new GridLayoutManager(this,3);
+
+        recyclerView.setLayoutManager(layoutManager);
+
+        Adapter = new MyAdpater(items,mContext);
+        recyclerView.setAdapter(Adapter);
+
+    }
+}
+
+class MyAdpater extends RecyclerView.Adapter<MyAdpater.ViewHolder> {
+    private Context context;
+    private ArrayList<Item> mItems;
+    private int lastPosition = -1;
+
+    public MyAdpater(ArrayList items, Context mContext) {
+        mItems = items;
+        context = mContext;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_cardview,parent,false);
+        ViewHolder holder = new ViewHolder(v);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+        holder.imageView.setImageResource(mItems.get(position).image);
+        holder.textView.setText(mItems.get(position).imagetitle);
+        setAnimation(holder.imageView, position);
+    }
+
+    @Override public int getItemCount() {
+        return mItems.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ImageView imageView;
+        public TextView textView; public ViewHolder(View view) {
+            super(view);
+            imageView = (ImageView) view.findViewById(R.id.cardimage);
+            textView = (TextView) view.findViewById(R.id.cardtext);
+        }
+    }
+
+    private void setAnimation(View viewToAnimate, int position) {
+        if (position > lastPosition) {
+            Animation animation = AnimationUtils.loadAnimation(context, android.R.anim.slide_in_left);
+            viewToAnimate.startAnimation(animation); lastPosition = position;
+        }
     }
 }
