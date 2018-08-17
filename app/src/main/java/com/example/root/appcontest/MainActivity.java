31d22928f308b62a4315fc6542002e6f8db7a3cc
fls2134 <fls213444@gmail.com>
2018-08-17 10:36:26 +0900

    change UI design

--- app/src/main/java/com/example/root/appcontest/MainActivity.java
+++ app/src/main/java/com/example/root/appcontest/MainActivity.java
@@ -1,197 +1,54 @@
 package com.example.root.appcontest;
 
-import android.Manifest;
-import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.view.View;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Button;
-import android.widget.Toast;
+import android.support.v7.widget.Toolbar;
 
-import static com.example.root.appcontest.R.id.nav_header_login;
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabSelectListener;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
-    final int MY_PERMISSIONS_REQUEST_CUR_PLACE = 3;
-    Intent mapsintent;
-    private int isLogin = 0;
-
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("이벤트");
 
         Window window = getWindow();
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         window.setStatusBarColor(getResources().getColor(R.color.statusbarColor));
 
-        /*Intents*/
-        mapsintent = new Intent(MainActivity.this, MapsActivity.class);
+        // 최초 화면 설정
+        FragmentTransaction fragmentTransaction
+                = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.action_container, new HomeFragment());
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
 
-        /**
-         *  button for move to newsfeed activity
-         */
-        Button newsfeed = (Button)findViewById(R.id.newsfeedButton);
-        newsfeed.setOnClickListener(new View.OnClickListener() {
+        // 하단 바 설정
+        BottomBar bottomBar = (BottomBar)findViewById(R.id.bottom_bar);
+        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, ScrollingActivity.class);
-                startActivity(intent);
+            public void onTabSelected(int tabId) {
+                FragmentTransaction fragmentTransaction
+                        = getSupportFragmentManager().beginTransaction();
+
+                switch (tabId) {
+                    case R.id.tab_home:
+                        fragmentTransaction.replace(R.id.action_container,
+                                new HomeFragment()).commit();
+                        break;
+                    case R.id.tab_location:
+                        fragmentTransaction.replace(R.id.action_container,
+                                new MapFragment()).commit();
+                        break;
+                }
             }
         });
-
-        Intent serviceIntent = new Intent(MainActivity.this, AlertService.class);
-        startService(serviceIntent);
-
-        /**
-         *  setting tool bar
-         */
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        /**
-         * add drawer this activity
-         */
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-
-        /**
-         * login controll
-         */
-        Button loginButton = (Button)findViewById(R.id.nav_loginbtn);
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //Intent intent = new Intent(MainActivity.this, "somewhere to go");
-                //startActivity(intent);
-            }
-        });
-
-        /**
-         * navigation view controll
-         */
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-        if(isLogin == 0) {
-            navigationView.inflateHeaderView(R.layout.nav_header_login);
-        }
-        else {
-            navigationView.inflateHeaderView(R.layout.nav_header_info);
-        }
-
-        /* maps activity button */
-        Button mapsButton = (Button) findViewById(R.id.gpsButton);
-        mapsButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                /* permission check required here
-                int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest, Manifest.permission.ACCESS_FINE_LOCATION);
-                if(permissionCheck == PackageManager.PERMISSION_DENIED)
-                {
-
-
-                }
-                */
-
-                int permissionCheck = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION);
-                if(permissionCheck != PackageManager.PERMISSION_GRANTED)
-                {
-                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_CUR_PLACE);
-                    //if(ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION))
-                }
-                else
-                {
-                    if(permissionCheck == PackageManager.PERMISSION_GRANTED)
-                        startActivity(mapsintent);
-                    else
-                        Toast.makeText(MainActivity.this, "위치 권한이 없어서 지도를 표시할 수 없습니다.", Toast.LENGTH_SHORT).show();
-                }
-
-
-            }
-        });
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode)
-        {
-            case MY_PERMISSIONS_REQUEST_CUR_PLACE:
-            {
-                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
-                    startActivity(mapsintent);
-                else
-                    Toast.makeText(MainActivity.this, "위치 권한이 없으면 지도를 표시할 수 없습니다.", Toast.LENGTH_SHORT).show();
-                return;
-            }
-
-        }
-    }
-
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_setting) {
-            Toast.makeText(getApplicationContext(), "버튼 눌림", Toast.LENGTH_LONG).show();
-            return true;
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
     }
 }
+
